5dfef829654bad1d5f9e9fe123bc3e3bc74f22c4
Jason Paryani <github@jparyani.com>
2015-07-09 15:06:55 -0700

    Add --smallfiles to mongodb creation

--- run_grain.sh
+++ run_grain.sh
@@ -7,6 +7,6 @@ CURRENT_VERSION="0"
 mkdir -p /var/uploads
 test -d /var/db || (mkdir -p /var/db && echo $CURRENT_VERSION > /var/VERSION)
 
-./niscud --fork --logpath /var/mongod.log --dbpath /var/db --noauth --bind_ip 127.0.0.1 --nohttpinterface --noprealloc
+./niscud --fork --logpath /var/mongod.log --dbpath /var/db --noauth --bind_ip 127.0.0.1 --nohttpinterface --noprealloc --smallfiles
 sleep 2
 LCB_HTTP_HOST=0.0.0.0 npm start
